f5c5e02bc8d5e98736d236ee41198f22c5405885
Manoth Chinthaka <noriskmc@gmail.com>
2025-10-23 15:36:17 +0530

    add: bineray search with recursion

--- src/server.ts
+++ src/server.ts
@@ -63,3 +63,26 @@ function binarySearch(arr: number[], target: number) {
 }
 
 console.log(binarySearch([1, 2, 3, 4, 5, 6], 6));
+
+// with recursive
+
+function recursiveBinarySearch(arr: number[], target: number) {
+  return search(arr, target, 0, arr.length - 1);
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (target === arr[middleIndex]) {
+    return middleIndex;
+  }
+  if (target < arr[middleIndex]) {
+    return search(arr, target, leftIndex, middleIndex - 1);
+  } else {
+    return search(arr, target, middleIndex + 1, rightIndex);
+  }
+}
+
+console.log(recursiveBinarySearch([1, 2, 3, 4, 5], 4));
