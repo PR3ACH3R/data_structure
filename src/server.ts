17552832b45e489417fe31ff25626a2e55d3712c
Manoth Chinthaka <noriskmc@gmail.com>
2025-10-23 14:06:02 +0530

    add: factorial of a number

--- src/server.ts
+++ src/server.ts
@@ -11,3 +11,15 @@ function fibonacci(n: number) {
 console.log(fibonacci(2)); // [0,1]
 console.log(fibonacci(3)); // [0,1,1]
 console.log(fibonacci(7)); // [0,1,1,2,3,5,8]
+
+//factorial of a number
+
+function factorial(n: number) {
+  let result = 1;
+  for (let i = 2; i < n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+console.log(factorial(5)); //24
